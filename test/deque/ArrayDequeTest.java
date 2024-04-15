772bbcf0dbbed39672fb7fb780da9016e50a9dff
phamphuc97 <phamphuc97@berkeley.edu>
2024-04-14 23:10:24 -0700

    Add more tests for ArrayDeque

--- test/deque/ArrayDequeTest.java
+++ test/deque/ArrayDequeTest.java
@@ -2,6 +2,8 @@ package deque;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
@@ -40,5 +42,23 @@ public class ArrayDequeTest {
         assertThrows(RuntimeException.class, () -> {
             intArrayDeque.get(4);
         });
+    }
+
+    @Test
+    /* Test the iterator */
+    public void testIterator() {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        for (int i = 0; i < 4; i++)
+            ad.addFirst(i);
+        for (int i = 4; i < 8; i++)
+            ad.addLast(i);
+        for (int i: ad)
+            System.out.print(i + " ");
+        System.out.println();
+        Iterator<Integer> iter = ad.iterator();
+        iter.next();
+        iter.next();
+        assertEquals(1, (int) iter.next());
+        assertEquals(0, (int) iter.next());
     }
 }
